de32a16a49559e1f756b7ffebeb2023b8ea023f4
Rishav Agarwal <rishavcoder@gmail.com>
2019-10-08 17:04:45 +0530

    ux: Show message sent message

--- client/src/components/WriteMessage.js
+++ client/src/components/WriteMessage.js
@@ -4,20 +4,39 @@ import { Button } from 'react-bootstrap';
 import axios from '../utils/axios';
 
 class WriteMessage extends Component {
-	state = { msg: '' };
+	state = {
+		msg: '',
+		showSent: false
+	};
 
 	static propTypes = {
 		username: PropType.string.isRequired
 	};
 
-	handleMsgChange = val => this.setState({ msg: val });
+	sentTimeout = null;
+
+	handleMsgChange = val => {
+		this.setState({ msg: val, showSent: false });
+		if (this.sentTimeout) clearTimeout(this.sentTimeout);
+	};
 
 	sendMessage = () => {
-		axios().post('/user/send', {
-			id: this.props.username,
-			message: this.state.msg
-		});
+		axios()
+			.post('/user/send', {
+				id: this.props.username,
+				message: this.state.msg
+			})
+			.then(res => {
+				this.setState({ msg: '', showSent: true });
+				this.sentTimeout = setTimeout(
+					() => this.setState({ showSent: false }),
+					3000
+				);
+			});
 	};
+
+	componentWillUnmount = () =>
+		this.sentTimeout ? clearTimeout(this.sentTimeout) : null;
 
 	render() {
 		return (
@@ -33,6 +52,15 @@ class WriteMessage extends Component {
 				<Button className="align-self-end my-2" onClick={this.sendMessage}>
 					Send
 				</Button>
+				<p
+					className="text-success"
+					style={{
+						fontSize: '0.8em',
+						visibility: this.state.showSent ? 'visible' : 'hidden'
+					}}
+				>
+					Thank you for sending the message!
+				</p>
 			</div>
 		);
 	}
